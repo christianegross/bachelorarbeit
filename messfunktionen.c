913ceac847bc0abe86769e11a034df7e2c9ec3b0
christianegross <chgross.wickede@gmail.com>
2020-06-04 11:24:42 +0200

    Skalierung: Auswertung ins Skript integriert(Mittelwerte, Varianzen),in sweep nowait und schedule dynamic eingefügt

--- messfunktionen.c
+++ messfunktionen.c
@@ -330,9 +330,10 @@ double sweep(int *gitter, int laenge, double j, double T, gsl_rng *generator, do
 	int changes =0;//misst Gesamtzahl der spinflips
 	int changesklein=0;//misst Spinflips in parallelen Thread
 	//schwarz: d1+d2 gerade
+	//int chunksize=(int)ceil((double)laenge/2.0/(double)omp_get_num_threads());
 	#pragma omp parallel firstprivate (delta, veraenderungH, changesklein, d1, d2) shared (H, changes)
 	{
-		#pragma omp for
+		#pragma omp for nowait schedule (dynamic) 
 		for (d1=0; d1<laenge;d1+=1){
 			for (d2=0; d2<laenge; d2+=1){//geht in zweiter dimension durch (alle Spalten einer Zeile)
 				if((d1+d2)%2==0){
@@ -359,7 +360,7 @@ double sweep(int *gitter, int laenge, double j, double T, gsl_rng *generator, do
 	//~ //weiss: d1+d2 ungerade, sonst analog zu schwarz
 	//~ #pragma omp parallel firstprivate (delta, veraenderungH, changesklein, d1, d2) shared (H, changes)
 	//~ {
-		#pragma omp for
+		#pragma omp for nowait schedule (dynamic)
 		for (d1=0; d1<laenge;d1+=1){
 			for (d2=0; d2<laenge; d2+=1){//geht in zweiter dimension durch (alle Spalten einer Zeile)
 				if((d1+d2)%2==0){
